3530eaab10689cb57cb45e7e479e5f6a60c71215
MrSirnik <nektotim21@gmail.com>
2025-09-27 18:21:45 +0300

    Serialization Assignment 2

---
+++ Assets/Scripts/EditorMover.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	
+	[RequireComponent(typeof(PositionSaver))]
+	public class EditorMover : MonoBehaviour
+	{
+		private PositionSaver _save;
+		private float _currentDelay;
+
+        //todo comment: Что произойдёт, если _delay > _duration?
+        //если _delay больше чем оставшееся время _duration, то последняя запись не сохранится
+        [Range(0.2f, 1.0f)]
+        [SerializeField] private float _delay = 0.2f;//через каждые _delay секунд 
+        [Min(0.2f)]
+        [SerializeField] private float _duration = 20f;//в течении времени _duration
+
+		private void Start()
+		{
+            //todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+            //GetComponent - относительно дорогой вызов. В Start() он выполняется один раз, в Update() - каждый кадр
+            _save = GetComponent<PositionSaver>();
+			_save.Records.Clear();
+
+            if (_duration <= _delay)
+            {
+                Debug.LogWarning($"Duration ({_duration}) is less or equal to delay ({_delay}). Setting duration to {_delay * 5f}");
+                _duration = _delay * 5f; // Устанавливаем в 5 раз больше _delay
+            }
+        }
+
+		private void Update()
+		{
+			_duration -= Time.deltaTime;
+			if (_duration <= 0f)
+			{
+                enabled = false;
+				Debug.Log($"<b>{name}</b> finished", this);
+				return;
+			}
+
+            //todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+            //потому что мы забудем значение _delay
+            _currentDelay -= Time.deltaTime;
+			if (_currentDelay <= 0f)
+			{
+				_currentDelay = _delay;
+				_save.Records.Add(new PositionSaver.Data
+				{
+					Position = transform.position,
+                    //todo comment: Для чего сохраняется значение игрового времени?
+                    //для фиксирования передвижения во времени
+                    Time = Time.time,
+				});
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ReplayMover.cs
@@ -0,0 +1,128 @@
+﻿//using System;
+//using UnityEngine;
+
+//namespace DefaultNamespace
+//{
+//	[RequireComponent(typeof(PositionSaver))]
+//	public class ReplayMover : MonoBehaviour
+//	{
+//		private PositionSaver _save;
+
+//		private int _index = 0;
+//		private PositionSaver.Data _prev;
+//		private float _duration;
+
+//        [SerializeField, Range(0.1f, 1f)]
+//        private float _speed = 0.5f; // Коэффициент замедления
+//        private Vector3 _currentPosition;
+
+//        private void Start()
+//		{
+//            ////todo comment: зачем нужны эти проверки?
+//            //это защитные проверки
+//            if (!TryGetComponent(out _save) || _save.Records.Count == 0)
+//			{
+//				Debug.LogError("Records incorrect value", this);
+//                //todo comment: Для чего выключается этот компонент?
+//                //чтобы предотвратить ошибки в Update()
+//                enabled = false;
+//			}
+//            else
+//            {
+//                _currentPosition = transform.position;
+//            }
+//        }
+
+//		private void Update()
+//		{
+//			var curr = _save.Records[_index];
+//            //todo comment: Что проверяет это условие (с какой целью)?
+//            //это тайминг-условие для перехода к следующей точке записи
+//            //if (Time.time > curr.Time)
+//            if (slowedTime > curr.Time)
+//            {
+//				_prev = curr;
+//				_index++;
+//                //todo comment: Для чего нужна эта проверка?
+//                //это защита от выхода за границы массива и окончание воспроизведения
+//                if (_index >= _save.Records.Count)
+//                {
+//                    enabled = false;
+//                    Debug.Log($"<b>{name}</b> finished", this);
+//                    return;
+//                }
+//            }
+//            //todo comment: Для чего производятся эти вычисления (как в дальнейшем они применяются)?
+//            //это нормализованное время для интерполяции между двумя точками
+//            var delta = (slowedTime - _prev.Time) / (curr.Time - _prev.Time);
+//            //todo comment: Зачем нужна эта проверка?
+//            //это защита от деления на ноль и некорректных значений
+//            if (float.IsNaN(delta)) delta = 0f;
+//            //todo comment: Опишите, что происходит в этой строчке так подробно, насколько это возможно
+//            //в этой строчке пишется комментарий с вопросом, но она пропускается при выполнении кода, поэтому ничего не происходит с ней или без нее
+//            //transform.position = Vector3.Lerp(_prev.Position, curr.Position, delta);
+//            Vector3 targetPosition = Vector3.Lerp(_prev.Position, curr.Position, delta);
+//            _currentPosition = Vector3.Lerp(_currentPosition, targetPosition, Time.deltaTime * 5f);
+//            transform.position = _currentPosition;
+//        }
+//	}
+//}
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    [RequireComponent(typeof(PositionSaver))]
+    public class ReplayMover : MonoBehaviour
+    {
+        private PositionSaver _save;
+        private int _index = 0;
+        private PositionSaver.Data _prev;
+
+        [SerializeField, Range(0.1f, 1f)]
+        private float _speed = 0.5f; // Коэффициент замедления
+        private Vector3 _currentPosition;
+
+        private void Start()
+        {
+            if (!TryGetComponent(out _save) || _save.Records.Count == 0)
+            {
+                Debug.LogError("Records incorrect value", this);
+                enabled = false;
+            }
+            else
+            {
+                _currentPosition = transform.position;
+            }
+        }
+
+        private void Update()
+        {
+            var curr = _save.Records[_index];
+
+            // Замедляем время в 2 раза (0.5f) или другое значение
+            float slowedTime = Time.time * _speed;
+
+            if (slowedTime > curr.Time)
+            {
+                _prev = curr;
+                _index++;
+
+                if (_index >= _save.Records.Count)
+                {
+                    enabled = false;
+                    Debug.Log($"<b>{name}</b> finished", this);
+                    return;
+                }
+            }
+
+            var delta = (slowedTime - _prev.Time) / (curr.Time - _prev.Time);
+            if (float.IsNaN(delta)) delta = 0f;
+
+            // Плавная интерполяция к целевой позиции
+            Vector3 targetPosition = Vector3.Lerp(_prev.Position, curr.Position, delta);
+            _currentPosition = Vector3.Lerp(_currentPosition, targetPosition, Time.deltaTime * 5f);
+            transform.position = _currentPosition;
+        }
+    }
+}
