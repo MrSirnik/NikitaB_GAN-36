18c3423b1e9f0fbd02f019f0588818d1ad2daabd
MrSirnik <nektotim21@gmail.com>
2025-10-20 19:29:08 +0300

    made the "Gates" script

--- Assets/Scripts/Gates.cs
+++ Assets/Scripts/Gates.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class Gates : MonoBehaviour
 {
+    [SerializeField] private string _tag = "Ball";
+    private int _score = 0;
+
+
     void OnTriggerEnter(Collider other)
     {
         // ѕровер€ем материал через тег или слой
-        if (other.CompareTag("Ball"))
+        if (other.CompareTag(_tag))
         {
             Destroy(other.gameObject);
+            _score++;
             //в консоль выводитс€ текущий игровой счет -->
+            Debug.Log($"√олов - {_score}");
         }
     }
 }
